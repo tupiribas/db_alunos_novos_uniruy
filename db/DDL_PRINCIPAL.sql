728534b5b75db7c7e605561697017469a52299c0
tupiribas <tupiguedes@outlook.com>
2023-10-15 15:20:32 -0300

    Alterando tipos de dados e regras de negócio

--- db/DDL_PRINCIPAL.sql
+++ db/DDL_PRINCIPAL.sql
@@ -2,8 +2,9 @@ CREATE DATABASE db_curso_uniruy;
 
 USE db_curso_uniruy;
 
-CREATE TABLE tb_cliente(
-	idcliente INT IDENTITY(1,1) NOT NULL,
+CREATE TABLE tb_pessoa(
+	idcliente INT IDENTITY(1,1) UNIQUE NOT NULL,
+	matricula varchar(30) UNIQUE NOT NULL,
 	nome VARCHAR(90) NOT NULL,
 	datanascimento DATETIME NOT NULL,
 	cpf VARCHAR(12) NOT NULL,
@@ -13,19 +14,47 @@ CREATE TABLE tb_cliente(
 );
 
 CREATE TABLE tb_curso(
-	idcurso INT IDENTITY(1,1) NOT NULL,
+	idcurso INT IDENTITY(1,1) UNIQUE NOT NULL,
 	nome VARCHAR(90) NOT NULL,
-	cargahoraria INT NOT NULL,
+	cargahoraria numeric NOT NULL,
 	descricao VARCHAR(120) NOT NULL,
 	PRIMARY KEY(idcurso)
 );
 
 CREATE TABLE tb_inscricao(
 	idcurso INT NOT NULL,
-	idcliente INT NOT NULL,
+	idpessoa INT NOT NULL,
 	datainscricao DATETIME NOT NULL,
 	datacancelamento DATETIME NOT NULL,
-	PRIMARY KEY(idcurso,idcliente),
+	PRIMARY KEY(idcurso,idpessoa),
 	FOREIGN KEY (idcurso) REFERENCES tb_curso (idcurso),
-	FOREIGN KEY (idcliente) REFERENCES tb_cliente (idcliente)
+	FOREIGN KEY (idpessoa) REFERENCES tb_pessoa (idcliente)
 );
+
+/* Alterações no projeto 
+	1. Nome da tabela "tb_curso" -"tb_pessoa";
+	2. Criar tabela tb_disciplina
+	3. Incrementando regra de negócio
+*/
+
+-- EXEC sp_rename "tb_cliente","tb_pessoa";
+
+CREATE TABLE tb_disciplina(
+	iddisciplina INT IDENTITY(1,1) UNIQUE NOT NULL,
+	codigoincricao VARCHAR(30) UNIQUE NOT NULL,
+	nome VARCHAR(90) NOT NULL,
+	descricao VARCHAR(120) NOT NULL,
+	cargahoraria NUMERIC NOT NULL,
+	idprereq INT NULL, -- Regra de negocio
+	idcurso INT NOT NULL,
+	PRIMARY KEY (iddisciplina),
+	FOREIGN KEY (idcurso) REFERENCES tb_curso (idcurso)
+);
+
+-- Regra de negocio: Caso o aluno esteja dependendo cursar outra disciplina para poder se matricular na outra.
+
+ALTER TABLE tb_disciplina
+ADD CONSTRAINT
+Fk_Disciplina_Prereq
+FOREIGN KEY (idprereq)
+REFERENCES tb_disciplina (iddisciplina);
